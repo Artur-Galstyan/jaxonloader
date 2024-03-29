7a2c9c6524f6a6c055634b5b0afcc5beacd1bbb9
Artur Galstyan <mail@arturgalstyan.dev>
2024-03-29 19:10:45 +0100

    fix device put start

--- jaxonloader/dataset.py
+++ jaxonloader/dataset.py
@@ -1,18 +1,24 @@
 import equinox as eqx
+import jax
 from jaxtyping import Array
+
+
+cpus = jax.devices("cpu")
+gpus = jax.devices("gpu")
 
 
 class JaxonDataset(eqx.Module):
     data: Array
 
     def __init__(self, data: Array):
-        self.data = data
+        self.data = jax.device_put(data, cpus[0])
 
     def __len__(self) -> int:
         return len(self.data)
 
     def __call__(self, idx: int) -> Array:
-        return self.data[idx]
+        data = jax.device_put(self.data, jax.devices())
+        return data[idx]
 
     def __getitem__(self, idx: int) -> Array:
         return self(idx)
